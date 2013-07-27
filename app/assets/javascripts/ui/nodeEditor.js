889f758188bd2bacc4e839a72ec7584cc7f4e2b8
jtournay <jtournay@linkedin.com>
2013-07-27 21:23:24 +0200

    draw using d3

--- app/assets/javascripts/ui/nodeEditor.js
+++ app/assets/javascripts/ui/nodeEditor.js
@@ -1,5 +1,3 @@
-
-
 zound.ui.NodeEditor = Backbone.View.extend({
   options: {
     w: 700,
@@ -25,7 +23,9 @@ zound.ui.NodeEditor = Backbone.View.extend({
   },
 
   init: function () {
-    this.paper = Raphael(this.el, this.options.w, this.options.h);
+    this.svg = d3.select("#node-editor").append("svg")
+      .attr("width", this.options.w)
+      .attr("height", this.options.h);
   },
 
   onModuleRemove: function (module) {
@@ -47,150 +47,82 @@ zound.ui.NodeEditor = Backbone.View.extend({
     }
   },
 
-  // FIXME TODO: split into multiple function for a better event binding
-  // we need to avoid always redrawing everything for performance
-  //
-  render: function () {
-    var self = this;
-    var paper = this.paper;
+  render: function(){
+    var g = this.draw(this.model.modules.models);
+  },
 
-    var W = this.options.w;
-    var H = this.options.h;
+  draw: function (data) {
+    var svg = this.svg,
+      options = {
+        margin: 15
+      };
 
-    var MARGIN_CIRCLE = 15;
+    var get = function(k){
+      return function(m){ return m.get(k); };
+    };
 
-    // draw links
-    this.model.modules.each(function (module) {
-      var startx = module.get("x");
-      var starty = module.get("y");
-
-      // FIXME, the connect lines are not supporting new connections yet
-      //if (!module.$outputs) {
-      if (module.$outputs) {
-        _.each(module.$outputs, function (path) {
-          path.remove();
+    var e = svg
+      .selectAll("g")
+      .data(data, get('id'))
+      .enter();
+    // connections
+    var cs = _.flatten(
+      _.map(data, function(m){
+        return _.map(m.outputs.models, function(o){
+          return [m, o];
         });
-      }
-        module.$outputs = module.outputs.map(function (out) {
-          var endx = out.get("x");
-          var endy = out.get("y");
-          var path = paper.path("M"+startx+" "+starty+"L"+endx+" "+endy);
-          path.attr("stroke", "#bfcbdb");
-          path.attr("stroke-width", 2);
-          path.toBack();
-          return path;
-        });
-      /*}
-      else {
-        _.each(_.zip(module.outputs.models, module.$outputs), function (o) {
-          var out = o[0];
-          var path = o[1];
-          var endx = out.get("x");
-          var endy = out.get("y") + out.get("h");
-          path.attr("path", "M"+startx+" "+starty+"L"+endx+" "+endy);
-        });
-      }*/
-    });
+      }), true);
 
-    // draw modules
-    this.model.modules.each(function (module) {
-      if (!module.$box) {
-        var x = module.get("x");
-        var y = module.get("y");
-        var w = module.get("w");
+    svg.selectAll('line')
+      .data(cs)
+      .enter()
+      .append("svg:line")
+      .attr("stroke-width", 2)
+      .attr("stroke", "#bfcbdb")
+      .attr("x1", function(m) { return m[0].get('x'); })
+      .attr("y1", function(m) { return m[0].get('y'); })
+      .attr("x2", function(m) { return m[1].get('x'); })
+      .attr("y2", function(m) { return m[1].get('y'); });
 
-        var all = paper.set();
+    var g = e
+      .append("svg:g")
+      .attr('id', get('id'));
+    // outer circle
+    g.append('svg:circle')
+      .attr('class', 'outer')
+      .attr("cx", get('x'))
+      .attr("cy", get('y'))
+      .attr("r", function(d){ return d.get('w') / 2 + options.margin; })
+      .attr("fill", "#f3f9ff")
+      .attr("stroke-width", 10)
+      .attr("stroke", "#bfcbdb");
+    // center circle
+    g.append('svg:circle')
+      .attr("cx", get('x'))
+      .attr("cy", get('y'))
+      .attr("r", function(d){ return d.get('w') / 2; })
+      .attr("fill", "#314355")
+      .attr("stroke-width", 0);
+    // title
+    g.append("svg:text")
+      .attr("x", get('x'))
+      .attr("y", get('y'))
+      .attr("fill", '#fff')
+      .attr("font-family", "monospace")
+      .attr("font-size", "10px")
+      .attr("text-anchor", "middle")
+      .text(get('title'));
+    // id
+    g.append("svg:text")
+      .attr("x", get('x'))
+      .attr("y", function(d){ return d.get('y') + 10; })
+      .attr("fill", "#fff")
+      .attr("text-anchor", "middle")
+      .attr("font-family", "monospace")
+      .attr("font-size", "8px")
+      .text(get('id'));
 
-        var bg = paper.circle(0, 0, w / 2 + MARGIN_CIRCLE);
-        bg.attr("fill", "#f3f9ff");
-        bg.attr("stroke-width", 10);
-        bg.attr("stroke", "#bfcbdb");
-
-        var box = paper.circle(0, 0, w / 2);
-        box.attr("fill", "#314355");
-        box.attr("stroke-width", 0);
-
-        var titleText = paper.text(0, -3, module.get("title"));
-        titleText.attr("fill", "#fff");
-
-        var numberText = paper.text(5, 7, module.getDisplayId());
-        numberText.attr("fill", "#fff");
-        numberText.attr("text-anchor", "end");
-        numberText.attr("font-family", "monospace");
-        numberText.attr("font-weight", "bold");
-
-        all.push(box, titleText, numberText, bg);
-
-        module.on("user-select", function (name) {
-          bg.attr("stroke", "#79838e");
-        });
-        module.on("user-unselect", function (name) {
-          bg.attr("stroke", "#bfcbdb");
-        });
-
-        // Handle drag of a module
-        (function (startX, startY) {
-          var draggable = paper.set(box, titleText);
-          draggable.attr("cursor", "move");
-          draggable.drag(function (dx, dy) {
-            x = Math.min(Math.max(startX + dx, 0), W-w);
-            y = Math.min(Math.max(startY + dy, 0), H-w);
-            module.set({ x: x, y: y });
-            all.transform("t"+[x, y]);
-          }, function () {
-            startX = x;
-            startY = y;
-            self.selectModule(module);
-          }, function () {
-            module.set({ x: x, y: y });
-          });
-        }(x+w/2, y+w/2));
-
-        // Handle for connecting with the dot
-        module.canHaveOutputs() && (function (startx, starty, endx, endy) {
-
-          var outputDotPath = paper.path("");
-          all.push(outputDotPath);
-
-          outputDotPath.attr("stroke", "#bfcbdb");
-          outputDotPath.attr("stroke-dasharray", "- ");
-          outputDotPath.attr("stroke-width", 2);
-          outputDotPath.toFront();
-
-          bg.drag(function (dx, dy, mx, my, e) {
-            var t = all[0].transform()[0],
-                realMouseX = e.offsetX,
-                realMouseY = e.offsetY;
-            endx = realMouseX - t[1];
-            endy = realMouseY - t[2];
-            outputDotPath.attr("path", "M"+0+" "+0+"L"+endx+" "+endy);
-          }, function (sx, sy) {
-          }, function () {
-            outputDotPath.attr("path", "");
-            var px = module.get("x")+endx;
-            var py = module.get("y")+endy;
-            var out = self.model.modules.find(function (m) {
-              if (m.get("x") < px && px < m.get("x")+m.get("w") &&
-                  m.get("y") < py && py < m.get("y")+m.get("w")) {
-                    return m.canHaveInputs();
-                  }
-            });
-            if (out && module !== out) {
-              if (module.outputs.contains(out))
-                module.disconnect(out);
-              else
-                module.connect(out);
-            }
-          });
-        }(0, 0));
-
-        // Set initial position
-        all.transform("t"+[x,y]);
-
-        // Save for cache
-        module.$box = all;
-      }
-    });
-
+    return g;
   }
+
 });
