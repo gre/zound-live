c681ce7605fdc612326e7f8a8ec814594103677c
Gaëtan Renaudeau <gre@zenexity.com>
2014-03-24 15:27:03 +0100

    LICENCE

--- app/assets/javascripts/dataSample.js
+++ app/assets/javascripts/dataSample.js
@@ -1,3 +1,20 @@
+/*
+ * This file is part of ZOUND live.
+ *
+ * Copyright 2014 Zengularity
+ *
+ * ZOUND live is free software: you can redistribute it and/or modify
+ * it under the terms of the AFFERO GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * ZOUND live is distributed "AS-IS" AND WITHOUT ANY WARRANTY OF ANY KIND,
+ * INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
+ * NON-INFRINGEMENT, OR FITNESS FOR A PARTICULAR PURPOSE. See
+ * the AFFERO GNU General Public License for the complete license terms.
+ *
+ * You should have received a copy of the AFFERO GNU General Public License
+ * along with ZOUND live.  If not, see <http://www.gnu.org/licenses/agpl-3.0.html>
+ */
 
 var SONG_SAMPLE_1 = {
   id: "song_1",
